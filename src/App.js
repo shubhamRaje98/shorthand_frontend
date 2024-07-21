6d97b4fc906a29c1c1a087ee36a4549cb259e1a4
divi2001 <deepashok20@gmail.com>
2024-07-22 00:45:54 +0530

    checkout mcshubhya

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 // Import your components
+import SubjectWiseResultSummary from './components/subjectWiseSummaryDash/subjectWiseSummaryDash';
 import Login from './components/centerAdminLogin/centerAdminLogin';
 import ExpertLogin from './components/expertLogin/expertLogin';
 import ExpertAdmin from './components/expertLogin/expertAdmin';
@@ -22,44 +23,35 @@ import AbsenteeRoll from './components/attendeeRoll/attendeeRoll';
 import ControllerPassword from './components/controllerPassword/controllerPassword';
 import FetchUpdateTable from './components/superAdmin/fetchUpdateTables';
 import PCRegistration from './components/pcRegistration/pcRegistration';
-import { io } from 'socket.io-client';
-import { SocketContext } from './SocketContext';
-
-const socket = io('http://localhost:3000');
-import SubjectWiseResultSummary from './components/subjectWiseSummaryDash/subjectWiseSummaryDash';
-
 
 axios.defaults.withCredentials = true;
 
 const App = () => {
     return (
-        <SocketContext.Provider value={socket}>
-            <DashboardProvider>
-                <Router>
-                    <ToastContainer />
-                    <Routes>
-                        <Route path="/" element={<Login />} />
-                        <Route path="/expert-login" element={<ExpertLogin />} />
-                        <Route path="/expertAdmin" element={<ExpertAdmin/>} />
-                        <Route path="/student-table" element={<StudentTable />} />
-                        <Route path="/home" element={<Home />} />
-                        <Route path="/expertDashboard" element={<ExpertDashboard />}>
-                            <Route index element={<SubjectSelection />} />
-                            <Route path=":subjectId" element={<QSet />} />
-                            <Route path=":subjectId/:qset" element={<FinalPassageTextlog />} />
-                        </Route>
-                        <Route path="/attendance-download" element={<AttendanceDownload />} />
-                        <Route path="/centerwise-student-count" element={<CenterwiseStudentCount />} />
-                        <Route path="/absentee-roll" element={<AbsenteeRoll />} />
-                        <Route path="/controller-password" element={<ControllerPassword />} />
-                        <Route path="/fetch-update-table" element={<FetchUpdateTable />} />
-                        <Route path="/fetch-pc-registration" element={<PCRegistration />} />
-                        <Route path="/result-subjectwise-summary" element={<SubjectWiseResultSummary />} />
-                    </Routes>
-                </Router>
-            </DashboardProvider>
-        </SocketContext.Provider>
-
+        <DashboardProvider>
+            <Router>
+                <ToastContainer />
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/expert-login" element={<ExpertLogin />} />
+                    <Route path="/expertAdmin" element={<ExpertAdmin/>} />
+                    <Route path="/student-table" element={<StudentTable />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/expertDashboard" element={<ExpertDashboard />}>
+                        <Route index element={<SubjectSelection />} />
+                        <Route path=":subjectId" element={<QSet />} />
+                        <Route path=":subjectId/:qset" element={<FinalPassageTextlog />} />
+                    </Route>
+                    <Route path="/attendance-download" element={<AttendanceDownload />} />
+                    <Route path="/centerwise-student-count" element={<CenterwiseStudentCount />} />
+                    <Route path="/absentee-roll" element={<AbsenteeRoll />} />
+                    <Route path="/controller-password" element={<ControllerPassword />} />
+                    <Route path="/fetch-update-table" element={<FetchUpdateTable />} />
+                    <Route path="/fetch-pc-registration" element={<PCRegistration />} />
+                    <Route path="/result-subjectwise-summary" element={<SubjectWiseResultSummary />} />
+                </Routes>
+            </Router>
+        </DashboardProvider>
     );
 };
 
